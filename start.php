e308ae50f70e92745f7adae29c30f24c788b8f4c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-07-28 11:15:21 +0200

    fix(manifest): fix manifest

--- start.php
+++ start.php
@@ -29,7 +29,7 @@ namespace hypeJunction\Lists;
 
 $plugin_id = basename(__DIR__);
 
-require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+require_once dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
 
 require_once __DIR__ . '/lib/hooks.php';
 
