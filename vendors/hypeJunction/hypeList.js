e2529c26bbde4358053b9f655d65b1f51cedaafa
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-01-24 19:32:46 +0100

    fix(js): doh, js event name

--- vendors/hypeJunction/hypeList.js
+++ vendors/hypeJunction/hypeList.js
@@ -196,7 +196,7 @@
 		init: function () {
 			var self = this;
 			$(self).trigger('ready');
-			self.$elem.trigger('change', [self.options]);
+			self.$elem.trigger('ready', [self.options]);
 		},
 		/**
 		 * Toggle visibility of items for visible pages
