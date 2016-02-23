3ddbf0140323bc9851299553b26eb72043d44a44
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-23 10:29:17 +0100

    feat(usability): rearrange handlers
    BREAKING CHANGE
    hypeJunction\Lists namespace has been dropped, and handlers have been
    prefixed with hypelists_ instead.
    lib/hooks.php has been merged with start.php

--- autoloader.php
+++ autoloader.php
@@ -5,5 +5,3 @@ if (file_exists("{$plugin_root}/vendor/autoload.php")) {
 	// check if composer dependencies are distributed with the plugin
 	require_once "{$plugin_root}/vendor/autoload.php";
 }
-
-require_once __DIR__ . '/lib/hooks.php';
