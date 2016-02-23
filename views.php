50b9597c08411bc42362f1692c5ce564a784d04a
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-23 10:21:32 +0100

    chore(views): move js views to components/list

--- views.php
+++ views.php
@@ -3,5 +3,7 @@
 return [
 	'default' => [
 		'hypeList.js' => __DIR__ . '/views/default/components/list.js',
+		'js/framework/lists/init.js' => __DIR__ . '/views/default/components/list/init.js',
+		'js/framework/lists/require' => __DIR__ . '/views/default/components/list/require.php',
 	]
 ];
