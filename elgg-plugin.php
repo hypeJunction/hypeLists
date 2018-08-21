333c8c83fdc66ba722a0fd304223ba5376ae6ea1
Jerome Bakker <jeabakker@coldtrick.com>
2018-08-21 10:44:49 +0200

    chore(lists): replaced views.php by elgg-plugin.php for future use

---
+++ elgg-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'views' => [
+		'default' => [
+			'hypeList.js' => __DIR__ . '/views/default/components/list.js',
+			'js/framework/lists/init.js' => __DIR__ . '/views/default/components/list/init.js',
+			'js/framework/lists/require' => __DIR__ . '/views/default/components/list/require.php',
+		],
+	],
+];
--- views.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	'default' => [
-		'hypeList.js' => __DIR__ . '/views/default/components/list.js',
-		'js/framework/lists/init.js' => __DIR__ . '/views/default/components/list/init.js',
-		'js/framework/lists/require' => __DIR__ . '/views/default/components/list/require.php',
-	]
-];
